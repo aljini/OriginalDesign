63945736585773dfba8d61feeafc6f3796b63dbd
unknown <rejohn@697-334-Bree.sf.k12.internal>
2015-08-24 12:35:10 -0700

    Made Hello Kitty base

--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -21,16 +21,16 @@ public void setup()
 }
 public void draw()
 {
+
+	noStroke();
+	rect(100,200,300,200);
 	strokeWeight(6);
-  	stroke(10);
- 	
- 	beginShape();
+  	stroke(200);
+ 	bezier(100,200,150,150,350,150,400,200);
+ 	bezier(100,200,50,250,50,350,100,400);
+ 	bezier(100,400,150,450,350,450,400,400);
+ 	bezier(400,200,450,250,450,350,400,400);
 
- 		curveVertex(200, 100);
- 		curveVertex(100, 400);
- 		curveVertex(300, 700);
-
- 	endShape();
 }
 
   static public void main(String[] passedArgs) {
