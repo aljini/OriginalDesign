a8667e882c32206f58518134ec129a4122f73900
unknown <rejohn@697-334-Bree.sf.k12.internal>
2015-08-25 11:37:14 -0700

    trying to make a ghost

--- OriginalDesign.pde
+++ OriginalDesign.pde
@@ -5,7 +5,17 @@ void setup()
 }
 void draw()
 {
+	capeHood();
+	head();
+	face();
 
+
+}
+
+//this is the basic head//
+void head()	
+{
+	fill(255);
 	noStroke();
 	rect(100,200,300,200);
 	strokeWeight(6);
@@ -14,6 +24,18 @@ void draw()
  	bezier(100,200,50,250,50,350,100,400);
  	bezier(100,400,150,450,350,450,400,400);
  	bezier(400,200,450,250,450,350,400,400);
+ }
 
+ //this will be the face//
+void face()
+{
+ 	
+ 	fill(0);
+ 	ellipse(160, 300, 40, 50);
+ 	ellipse(340, 300, 40, 50);
 }
 
+void capeHood()
+{
+	ellipse(300,200,300,200);
+}
--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -21,7 +21,17 @@ public void setup()
 }
 public void draw()
 {
+	capeHood();
+	head();
+	face();
 
+
+}
+
+//this is the basic head//
+public void head()	
+{
+	fill(255);
 	noStroke();
 	rect(100,200,300,200);
 	strokeWeight(6);
@@ -30,9 +40,21 @@ public void draw()
  	bezier(100,200,50,250,50,350,100,400);
  	bezier(100,400,150,450,350,450,400,400);
  	bezier(400,200,450,250,450,350,400,400);
+ }
 
+ //this will be the face//
+public void face()
+{
+ 	
+ 	fill(0);
+ 	ellipse(160, 300, 40, 50);
+ 	ellipse(340, 300, 40, 50);
 }
 
+public void capeHood()
+{
+	ellipse(300,200,300,200);
+}
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "OriginalDesign" };
     if (passedArgs != null) {
