a32a9384693efee1af142c5d310e67f87e4d2fb7
unknown <rejohn@697-334-Bree.sf.k12.internal>
2015-08-27 11:38:17 -0700

    trying to jump

--- OriginalDesign.pde
+++ OriginalDesign.pde
@@ -1,26 +1,53 @@
+boolean up =false;
+
+
 void setup()
 {
+	frameRate(20);
 	size(500, 750);
-	background(252,202,252);
+	
 }
 void draw()
 {
-	
+
+	if(up==true){
+		
+		y=y-5;
+
+	}
+
+	if(y<100){
+		
+		up=false;
+
+		while(y<200){
+
+				y=y+1;
+			}
+
+		}
+	}
+
+
+
+	background(252,202,252);
 	head();
 	face();
 
-
 }
+
+
+
 
 //this is the basic head//
 void head()	
 {
 	fill(255);
 	noStroke();
-	rect(100,y,300,y);
+	rect(100,y,300,200);
 	strokeWeight(6);
   	stroke(199,187,175);
- 	bezier(100,y,150,y-50,350,y-50,400,200);
+ 	bezier(100,y,150,y-50,350,y-50,400,y);
  	bezier(100,y,50,y+50,50,y+150,100,y+200);
  	bezier(100,y+200,150,y+250,350,y+250,400,y+200);
  	bezier(400,y,450,y+50,450,y+150,400,y+200);
@@ -34,14 +61,21 @@ void face()
  	ellipse(160, y+90, 40, 50);
  	ellipse(340, y+90, 40, 50);
  	noFill();
- 	bezier(180, 350, 197.5, 375, 232.5, 375, 250, 350);
- 	bezier(250, 350, 267.5, 375, 302.5, 375, 320, 350);
+ 	bezier(180, y+150, 197.5, y+175, 232.5, y+175, 250, y+150);
+ 	bezier(250, y+150, 267.5, y+175, 302.5, y+175, 320, y+150);
 
 }
 
 int y =200;
 
+
 void mousePressed ()
 {
 
+	up=true;
+
+
+
+  
+	
 }
--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -14,29 +14,58 @@ import java.io.IOException;
 
 public class OriginalDesign extends PApplet {
 
+boolean up =false;
+
+
 public void setup()
 {
+	frameRate(20);
 	size(500, 750);
-	background(252,202,252);
+	
 }
 public void draw()
 {
-	
+
+	if(up==true){
+		
+		y=y-5;
+
+	}
+
+	if(y<100){
+		
+		up=false;
+
+		while(y<200){
+
+			if(y<200){
+
+				y=y+1;
+			}
+
+		}
+	}
+
+
+
+	background(252,202,252);
 	head();
 	face();
 
-
 }
+
+
+
 
 //this is the basic head//
 public void head()	
 {
 	fill(255);
 	noStroke();
-	rect(100,y,300,y);
+	rect(100,y,300,200);
 	strokeWeight(6);
   	stroke(199,187,175);
- 	bezier(100,y,150,y-50,350,y-50,400,200);
+ 	bezier(100,y,150,y-50,350,y-50,400,y);
  	bezier(100,y,50,y+50,50,y+150,100,y+200);
  	bezier(100,y+200,150,y+250,350,y+250,400,y+200);
  	bezier(400,y,450,y+50,450,y+150,400,y+200);
@@ -50,16 +79,23 @@ public void face()
  	ellipse(160, y+90, 40, 50);
  	ellipse(340, y+90, 40, 50);
  	noFill();
- 	bezier(180, 350, 197.5f, 375, 232.5f, 375, 250, 350);
- 	bezier(250, 350, 267.5f, 375, 302.5f, 375, 320, 350);
+ 	bezier(180, y+150, 197.5f, y+175, 232.5f, y+175, 250, y+150);
+ 	bezier(250, y+150, 267.5f, y+175, 302.5f, y+175, 320, y+150);
 
 }
 
 int y =200;
 
+
 public void mousePressed ()
 {
 
+	up=true;
+
+
+
+  
+	
 }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "OriginalDesign" };
